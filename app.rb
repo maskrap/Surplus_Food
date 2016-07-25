effe96c765c343833ebbb550e291260a46f1c0ef
cory olson and matt wilkin <cory+matt@>
2016-07-25 16:33:35 -0700

    add user integration templates

--- app.rb
+++ app.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ENV['RACK_ENV'] = 'development'
 require("bundler/setup")
 
@@ -27,12 +26,66 @@ end
 
 before do
   @user = session[:user_id] ? User.find(session[:user_id]) : nil
-=======
-require "bundler/setup"
-Bundler.require :default
-Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+end
 
-get '/postings/new' do
-  erb(:posting_form.erb)
->>>>>>> eacf8c5ee8c4e21f69b608dfd988ee36e58aa0fb
+get "/" do
+  erb :index
+end
+
+get '/postings/new', :auth => :user do
+  erb(:posting_form)
+end
+
+
+get '/login' do
+ erb :login
+end
+
+post '/login' do
+  user = params[:username]
+  pw = params[:password]
+  new_user = User.find_by(name: user)
+  if new_user.try(:authenticate, pw)
+    session[:user_id] = new_user.id
+    redirect to '/'
+  else
+    flash[:notice] = "Invalid username or password"
+    redirect back
+  end
+end
+
+get '/signup' do
+  erb :signup
+end
+
+get '/users', :auth => :user  do
+  @user = User.find(session[:user_id])
+
+end
+
+post '/users/new' do
+  new_user = User.new(name: params[:email], password: params[:password])
+  if new_user.save
+    session[:user_id] = new_user.id
+    redirect to "/"
+  else
+    flash[:notice] = "Error creating account. Please check your input."
+    redirect back
+  end
+end
+
+get '/inboxes', :auth => :user do
+  @user = User.find(session[:user_id])
+  erb :inbox
+end
+
+get '/inboxes/:id', :auth => :user do
+  @user = User.find(session[:user_id])
+  @message = @users.messages.find_one(params[:id])
+  erb :message
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to '/'
 end
