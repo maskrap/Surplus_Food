96e1b5b136027ca5e7bae44b7ce46e7f243fbc9e
Leah Nelson <leahcnels@gmail.com>
2016-07-26 13:20:44 -0700

    refactor add categories to make it slightly less horrendous

--- app.rb
+++ app.rb
@@ -56,7 +56,7 @@ delete '/postings/:id', :auth => :user do
 end
 
 post '/postings/form' do
-  categories = Category.all
+  @categories = Category.all
   user_id = session[:user_id]
   description = params['description']
   source_type = params['source_type']
@@ -64,17 +64,14 @@ post '/postings/form' do
   location = params['location']
   @posting = Posting.create({user_id: user_id, description: description, source_type: source_type, quantity: quantity, location: location})
   form_categories = params['category_name'].split(", ")
-  if params['category_id'].to_i != nil && params['category_id'].to_i != 0
-    selected_category = Category.find(params['category_id'].to_i)
-    @posting.categories.push(selected_category)
-    form_categories.each { |category|
-      @posting.categories.push(Category.create({name: category}))
-    }
-  else
-    form_categories.each { |category|
-      @posting.categories.push(Category.create({name: category}))
-    }
-  end
+  form_categories.each { |category|
+    new_cat = Category.create({name: category})
+    if new_cat.save
+      @posting.categories.push(new_cat)
+    else
+      @posting.categories.push(Category.where(name: category))
+    end
+  }
   redirect '/postings'
 end
 
