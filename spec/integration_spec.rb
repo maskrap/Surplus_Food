a0122de38a6aa87c0b2e3d18870dfb453da6d90c
Sam Park <sampark18@gmail.com>
2016-07-27 15:40:24 -0700

    Add additional integration spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -7,6 +7,15 @@ describe 'the surplus food path', type: :feature do
     expect(page).to have_content 'Please sign in'
   end
 
+  it 'creates user' do
+    visit '/'
+    click_link 'Login'
+    new_user = User.create({:name => "Peter", :password => "Pan"})
+    page.set_rack_session(user_id: new_user.id)
+    visit '/'
+    expect(page).to have_content('PDX')
+  end
+
   it 'creates posting' do
     visit '/'
     click_link 'Login'
@@ -22,6 +31,29 @@ describe 'the surplus food path', type: :feature do
     fill_in 'quantity', {:with => "1 bushel"}
     select 'Northeast Portland', :from => "location"
     click_button 'Add'
+    expect(page).to have_content('Strawbs')
+  end
+
+  it 'allows user to message user of posting' do
+    visit '/'
+    click_link 'Login'
+    new_user = User.create({:name => "Peter", :password => "Pan"})
+    page.set_rack_session(user_id: new_user.id)
+    visit '/'
+    click_link 'add new posting'
+    test_posting = Posting.new({:description => "description"})
+    test_posting.save
+    fill_in 'description', {:with => "Strawbs"}
+    fill_in 'category_name', {:with => "Fruit"}
+    select 'Individual', :from => "source_type"
+    fill_in 'quantity', {:with => "1 bushel"}
+    select 'Northeast Portland', :from => "location"
+    click_button 'Add'
+    page.set_rack_session(user_id: nil)
+    new_user2 = User.create({:name => "John", :password => "Smith"})
+    page.set_rack_session(user_id: new_user2.id)
+    visit '/'
+    click_link 'postings1'
     expect(page).to have_content('Strawbs')
   end
 
